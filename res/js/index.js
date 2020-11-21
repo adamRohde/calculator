91c9560e9412643291da9ce6d6b1ca797aa61104
Adam Rohde <adamrohde@outlook.com>
2020-11-20 23:37:02 -0600

    second commit

--- res/js/index.js
+++ res/js/index.js
@@ -1,18 +1,42 @@
-// window.onload = function () {
-//     function numberClicked(number) {
-//         console.log(number);
-//     }
-// };
+//console.log("Hello there");
 
 const inputField = document.getElementById("display-field");
+const historyField = document.getElementById("history-field");
+
+let currentValueInFocus;
+let previousValueNotFocused;
+
+const operatorCharacters = ["/", "*", "-", "+"];
 
 function numberClicked(number) {
-    let numberContainer;
-    numberContainer = inputField.textContent + number;
-    inputField.textContent = numberContainer;
+    checkForIncompleteExpression();
+    updateHistoryField(number);
+    currentValueInFocus = inputField.textContent + number;
+    inputField.textContent = currentValueInFocus;
+}
+
+function operatorClicked(operator) {
+    updateHistoryField(operator);
+}
+
+function updateHistoryField(input) {
+    historyField.textContent = historyField.textContent + input;
+}
+
+function equalsButton() {
+    inputField.textContent = eval(historyField.textContent);
+}
+
+function checkForIncompleteExpression() {
+    if (operatorCharacters.includes(historyField.textContent.slice(-1))) {
+        console.log("Hello there");
+        inputField.textContent = " ";
+    } else {
+        console.log("byte there");
+    }
 }
 
 function clearInputField() {
     inputField.textContent = " ";
-    console.log("Hello there");
+    historyField.textContent = " ";
 }
